37c5fe33626c055928a3a7e6c43aaa0df7011f40
rkrocha <rafaelkr7@gmail.com>
2021-12-13 14:57:34 -0300

    added Welcome and Products pages, added Routes in App, and Browser Router to index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
